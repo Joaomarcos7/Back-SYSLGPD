14bac5061b422b29efaab58ce1758c2283b6e02c
João Marcos <joao.chaves@academico.ifpb.edu.br>
2023-11-04 14:38:19 -0300

    first commit

---
+++ src/test/java/com/syslgpd/SYSLGPD/SyslgpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syslgpd.SYSLGPD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyslgpdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
